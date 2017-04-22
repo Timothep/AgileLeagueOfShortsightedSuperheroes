d8f1132a0b6612b4080b2d9c7aba98803854a442
Timothée Bourguignon <timothee.bourguignon@mathema.de>
2017-04-22 23:45:29 +0200

    Initial commit

---
+++ deck.rb
@@ -0,0 +1,42 @@
+require 'squib'
+require 'pp'
+
+#Squib::logger.level = Logger::DEBUG
+
+data = Squib.xlsx file: 'heroes.xlsx'
+
+#def plusify(inputArray)
+#  return  inputArray.map{|num| num == nil || (not(num.is_a? Integer)) || num == 0 ? num : num < 0 ? num.to_s.insert(1, ' ') : '+ ' + num.to_s}
+#end
+
+#def CompleteSVGPath(inputArray)
+#  return  inputArray.map{|svg| svg == nil ? nil : 'svg\\' + svg}
+#end
+
+####################################
+############ CARDS DECK ############
+####################################
+
+Squib::Deck.new cards: data['Name'].size, layout: 'heroes.yml' do
+  background color: 'white'
+
+  # Background
+  svg layout: 'Background', file: data['Background']
+
+  # Avatar
+  svg layout: 'Avatar', file: data['Avatar']
+
+  # Text
+  text str: data['Plus'], layout: 'Plus'
+  text str: data['Minus'], layout: 'Minus'
+
+  # Title
+  text str: data['Name'], layout:'Name'
+
+  #rect layout: 'safe' # safe zone as defined by TheGameCrafter
+  #rect layout: 'cut' # cut line as defined by TheGameCrafter
+
+  #text str: Time.now, layout: 'credits'
+  save_png prefix: 'card_'
+  #save_pdf trim: 37.5, file: 'cards.pdf'
+end
