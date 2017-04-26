244e2b005adbfd6b01d7005c7b396a1a5410fea1
Timothée Bourguignon <timothee.bourguignon@mathema.de>
2017-04-26 22:35:34 +0200

    Advanced version

--- deck.rb
+++ deck.rb
@@ -21,10 +21,13 @@ Squib::Deck.new cards: data['Name'].size, layout: 'heroes.yml' do
   background color: 'white'
 
   # Background
-  svg layout: 'Background', file: data['Background']
+  svg layout: 'Background_cut', file: data['Background']
 
   #text_underlay
   svg layout: 'Background', file: 'svg/text_underlay.svg'
+
+  # but
+  text str: 'but...', layout: 'but_text'
 
   # Text
   text str: data['Plus'], layout: 'Plus'
@@ -33,8 +36,10 @@ Squib::Deck.new cards: data['Name'].size, layout: 'heroes.yml' do
   # Title
   text str: data['Name'], layout:'Name'
 
+  svg layout: 'Background', file: 'svg/border.svg'
+
   #rect layout: 'safe' # safe zone as defined by TheGameCrafter
-  #rect layout: 'cut' # cut line as defined by TheGameCrafter
+  rect layout: 'cut' # cut line as defined by TheGameCrafter
 
   #text str: Time.now, layout: 'credits'
   save_png prefix: 'card_'
